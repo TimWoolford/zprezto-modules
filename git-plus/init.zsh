47334c9ff5d9f61db33f58f4aed64d4696c9d80a
Tim Woolford <tim.woolford@gmail.com>
2020-10-25 16:35:59 +0000

    TECH [TimW] :: update to git-plus for varied repo structure

---
+++ git-plus/init.zsh
@@ -0,0 +1,16 @@
+typeset -gA REPOS
+
+zstyle -a ':prezto:module:git-plus' repos 'REPOS'
+zstyle -s ':prezto:module:git-plus' checkout-dir '_checkout_dir'
+zstyle -s ':prezto:module:git-plus' default-repo '_default_repo'
+
+typeset -g CHECKOUT_DIR=${_checkout_dir:-${HOME}}
+typeset -g DEFAULT_REPO=${_default_repo:-'github.com'}
+
+for repo linedata in ${(@kv)REPOS}; do
+  IFS="|" read conn spath <<< ${linedata}
+  for i in ${(@s/ /)spath:="*"}; do
+    local repoPath=${_checkout_dir}/${repo}/${i}
+    cdpath+=(${~repoPath})
+  done
+done
