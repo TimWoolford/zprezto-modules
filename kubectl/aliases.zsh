a403914babdb7f3d6fa4ea4daf338678130a7eb4
Tim Woolford <tim.woolford@gmail.com>
2020-11-10 21:24:18 +0000

    kubectl alias

--- kubectl/aliases.zsh
+++ kubectl/aliases.zsh
@@ -1,6 +1,7 @@
 alias kg='kubectl get'
 alias kctx='kubectx'
 alias kns='kubens'
+alias k='kubectl'
 alias wkg='watch kubectl get'
 alias kl='kubectl log -f'
-alias kpl='kubectl plugin login'
+alias kpl='kubectl login'
