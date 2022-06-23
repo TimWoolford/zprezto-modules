5436fb5e9c990cdd1d272be2a05d08ea86ee35c6
Tim Woolford <tim.woolford@gmail.com>
2022-06-23 16:13:15 +0100

    prompt monitors for updates

--- update-env/init.zsh
+++ update-env/init.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 
+typeset -g  _UPDATE_ENV_DIR=${0:h}
+typeset -gi _UPDATE_ENV_STATUS=0
+typeset -gi _SESSION_UPDATE_TIME=$(date +%s)
+typeset -gi _UPDATE_DAYS
+
 source "${0:h}/alias.zsh"
 source "${0:h}/updateCheck.zsh"
--- update-env/updateCheck.zsh
+++ update-env/updateCheck.zsh
@@ -1,51 +1,27 @@
 #!/usr/bin/env zsh
-typeset -g _UPDATE_ENV_STATUS=0
 # 0 - no update
 # 1 - over 1 day without check
 # 2 - upstream changes detected
 # 3 - local changes detected
 # 4 - over 7 days without check
 
-function timeSinceLastUpdate {
-  typeset -i last_update=0
+function daysSinceLastUpdate {
+  typeset -i lastUpdate=0 daySeconds=$((24 * 60 * 60))
 
-  [ -f ~/.environment_lastupdate ] && last_update=$(cat ~/.environment_lastupdate)
+  [ -f ~/.environment_lastupdate ] && lastUpdate=$(cat ~/.environment_lastupdate)
 
-  echo $(($(date +%s) - last_update))
-}
-
-function checkForEnvUpdates {
-  local BASE UPSTREAM REMOTE LOCAL
-  UPSTREAM=$(git -C "${ZDOTDIR}" rev-parse '@{u}')
-  LOCAL=$(git -C "${ZDOTDIR}" rev-parse HEAD)
-  REMOTE=$(git -C "${ZDOTDIR}" rev-parse "$UPSTREAM")
-  BASE=$(git -C "${ZDOTDIR}" merge-base HEAD "$UPSTREAM")
-
-  if [[ ${REMOTE} == ${LOCAL} ]]; then
-    _UPDATE_ENV_STATUS=1
-  else
-    if [[ ${LOCAL} == ${BASE} ]]; then
-      _UPDATE_ENV_STATUS=2
-    elif [[ ${REMOTE} == ${BASE} ]]; then
-      _UPDATE_ENV_STATUS=3
-    else
-      _UPDATE_ENV_STATUS=4
-    fi
-
-    [[ ${__p9k_instant_prompt_active} ]] || echo "${FX[bold]}${FG[yellow]}Your ${ZDOTDIR} is out of sync${FX[none]}${FG[none]}"
-  fi
+  echo $(( ($(date +%s) - lastUpdate) / daySeconds))
 }
 
 function startUpdateProcess {
-  typeset -i updateThresholdDays=${1:-6} \
-             day_seconds=$((24 * 60 * 60))
-             time_since_update=$(timeSinceLastUpdate)
-  typeset -i update_frequency=$((day_seconds * updateThresholdDays))
+  typeset -i errorThresholdDays=${1:-6} \
+             warnThresholdDays=${2:-1}
 
-  if [[ time_since_update -gt update_frequency ]]; then
-    _UPDATE_ENV_STATUS=4
+    _UPDATE_DAYS=$(daysSinceLastUpdate)
+
+  if [[ _UPDATE_DAYS -ge errorThresholdDays ]]; then
     if [[ ! ${__p9k_instant_prompt_active} ]]; then
-      echo "${FX[bold]}${FG[red]}Environment not updated for $((time_since_update / day_seconds)) days.${FX[none]}${FG[none]}"
+      echo "${FX[bold]}${FG[red]}Environment not updated for ${_UPDATE_DAYS} days.${FX[none]}${FG[none]}"
       typeset line
       vared -p "Would you like to check for updates? [Y/n]: " -c line
       if [[ "${line}" =~ ^(Y|y)$ ]]; then
@@ -53,19 +29,19 @@ function startUpdateProcess {
       else
         printf "\n${FG[red]}%s${FG[none]}\n\n" "You have annoyed the monkey!"
       fi
+    else
+      _UPDATE_ENV_STATUS=$(checkForEnvUpdates)
+      [[ ${__p9k_instant_prompt_active} && $_UPDATE_ENV_STATUS == 0 ]] || echo "${FX[bold]}${FG[yellow]}Your ${ZDOTDIR} is out of sync${FX[none]}${FG[none]}"
     fi
-  elif [[ time_since_update -gt day_seconds ]]; then
-    _UPDATE_ENV_STATUS=1
-    [[ ${__p9k_instant_prompt_active} ]] || echo "It has been ${FX[bold]}$((time_since_update / day_seconds))${FX[none]} days since your environment was updated"
-    checkForEnvUpdates
+  elif [[ _UPDATE_DAYS -ge warnThresholdDays ]]; then
+    _UPDATE_ENV_STATUS=$(checkForEnvUpdates)
+    [[ ${__p9k_instant_prompt_active} ]] || echo "It has been ${FX[bold]}${_UPDATE_DAYS}${FX[none]} days since your environment was updated"
   fi
 }
 
 {
-  typeset -g _UPDATE_ENV_DIR=${0:h}
   startUpdateProcess 6
 } always {
-  unfunction timeSinceLastUpdate
-  unfunction checkForEnvUpdates
+  unfunction daysSinceLastUpdate
   unfunction startUpdateProcess
 }
