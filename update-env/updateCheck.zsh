527e4555b04acea005fcda1eb8f8bd530ed448c0
Tim Woolford <tim.woolford@gmail.com>
2022-06-29 11:55:37 +0100

    make update checking less intrusive.?

--- update-env/updateCheck.zsh
+++ update-env/updateCheck.zsh
@@ -1,9 +1,4 @@
 #!/usr/bin/env zsh
-# 0 - no update
-# 1 - over 1 day without check
-# 2 - upstream changes detected
-# 3 - local changes detected
-# 4 - over 7 days without check
 
 function daysSinceLastUpdate {
   typeset -i lastUpdate=0 daySeconds
@@ -29,13 +24,13 @@ function startUpdateProcess {
       else
         echo
         subTitle "You have annoyed the monkey!" red
-        zsh-defer checkForEnvUpdates
+        checkForEnvUpdates
       fi
     else
       [[ ${__p9k_instant_prompt_active} && $_UPDATE_ENV_STATUS == 0 ]] || stageTitle "Your ${ZDOTDIR} is out of sync" yellow
     fi
   elif [[ _UPDATE_DAYS -ge warnThresholdDays ]]; then
-    zsh-defer checkForEnvUpdates
+    checkForEnvUpdates
     [[ ${__p9k_instant_prompt_active} ]] || echo "It has been ${FX[bold]}${_UPDATE_DAYS}${FX[none]} days since your environment was updated"
   fi
 }
